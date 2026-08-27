75358ae1ccc59ad1d0b5030a88fbdc9d867012f8
Zayne Fritz <zjf0010@auburn.edu>
2026-08-27 10:02:44 -0500

    Initialize Starter_Object and print values
    Added a Starter_Object instance and initialized values.

--- main.cpp
+++ main.cpp
@@ -8,4 +8,12 @@ int main()
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	Starter_Object object1;
+
+	object1.val1(67);
+	object1.val2(41);
+
+	cout<<"Object 1: " <<endl
+	cout<<"Value 1: " << object1.val1() <<endl
+	cout<<"Value 2: " << object1.val2() <<endl
 }
